c34409df56b656e2e37ba2f1dc534b46c112a217
Yann Viens <viensyann@gmail.com>
2016-12-03 08:23:06 +0100

    [DEV] Android Studio initial project.

---
+++ app/src/main/java/com/farmdrop/challenge/producers/ProducersListActivity.java
@@ -0,0 +1,13 @@
+package com.farmdrop.challenge.producers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProducersListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producers_list);
+    }
+}
