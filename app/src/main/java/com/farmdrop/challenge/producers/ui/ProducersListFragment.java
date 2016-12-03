2639270717e6dbe210105454ec4efa716d4ccd1d
Yann Viens <viensyann@gmail.com>
2016-12-03 18:18:41 +0100

    [DEV] Display ProgressBar while loading producers.

--- app/src/main/java/com/farmdrop/challenge/producers/ui/ProducersListFragment.java
+++ app/src/main/java/com/farmdrop/challenge/producers/ui/ProducersListFragment.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.farmdrop.challenge.producers.R;
 import com.farmdrop.challenge.producers.model.Producer;
@@ -24,6 +26,9 @@ public class ProducersListFragment extends Fragment {
     @BindView(R.id.fragment_producers_list_recycler_view)
     RecyclerView mRecyclerView;
 
+    @BindView(R.id.fragment_producers_list_progress_bar)
+    ProgressBar mProgressBar;
+
     private ProducersRecyclerViewAdapter mAdapter;
 
     @Nullable
@@ -33,11 +38,16 @@ public class ProducersListFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_producers_list, container);
         ButterKnife.bind(this, rootView);
         initRecyclerView(getContext());
+        initProgressBar();
         return rootView;
     }
 
+    @UiThread
     public void displayProducers(@NonNull List<Producer> producersList) {
         if (mAdapter != null) {
+            mProgressBar.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.VISIBLE);
+
             mAdapter.setProducersList(producersList);
             mAdapter.notifyDataSetChanged();
         } else {
@@ -54,5 +64,12 @@ public class ProducersListFragment extends Fragment {
 
         mAdapter = new ProducersRecyclerViewAdapter(context, mProducerList);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void initProgressBar() {
+        if (mProducerList == null || mProducerList.isEmpty()) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.GONE);
+        }
     }
 }
