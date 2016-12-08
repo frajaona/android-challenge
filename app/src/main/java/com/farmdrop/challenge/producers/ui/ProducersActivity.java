54068e71d0456d7c765043cec6ab133aee3fd042
Yann Viens <yann.viens@parrot.com>
2016-12-08 14:17:45 +0100

    [DEV] Make search works correctly with Activty lifecycle.

--- app/src/main/java/com/farmdrop/challenge/producers/ui/ProducersActivity.java
+++ app/src/main/java/com/farmdrop/challenge/producers/ui/ProducersActivity.java
@@ -3,9 +3,11 @@ package com.farmdrop.challenge.producers.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,6 +23,8 @@ import java.util.List;
 import javax.inject.Inject;
 
 public class ProducersActivity extends AppCompatActivity {
+    private static final String KEY_QUERY = "KEY_QUERY";
+
     @Inject
     ProducersListPresenter mPresenter;
 
@@ -28,7 +32,11 @@ public class ProducersActivity extends AppCompatActivity {
 
     private ProducerDetailsFragment mProducerDetailsFragment;
 
+    @Nullable
     private SearchView mSearchView;
+
+    @Nullable
+    private String mLastQuery;
 
     @NonNull
     private final OnProducersListActionListener mOnProducersListActionListener = new OnProducersListActionListener() {
@@ -55,12 +63,17 @@ public class ProducersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_producers);
         mProducersListFragment = (ProducersListFragment) getSupportFragmentManager().findFragmentById(R.id.activity_producers_fragment_list);
         mProducerDetailsFragment = (ProducerDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.activity_producers_fragment_details);
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(KEY_QUERY)) {
+            mLastQuery = savedInstanceState.getString(KEY_QUERY);
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
         mPresenter.registerListener(new ProducersListenerImpl());
+        restoreLastSearch();
     }
 
     @Override
@@ -71,13 +84,14 @@ public class ProducersActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (!mSearchView.isIconified()) {
+        if (mSearchView != null && !mSearchView.isIconified()) {
             mSearchView.setIconified(true);
         } else {
             super.onBackPressed();
         }
     }
 
+    @SuppressWarnings("ConstantConditions") // mSearchView can't be null in this method.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_search, menu);
@@ -86,13 +100,13 @@ public class ProducersActivity extends AppCompatActivity {
         mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                searchProducers(query);
+                searchProducers(query.trim());
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                searchProducers(newText);
+                searchProducers(newText.trim());
                 return true;
             }
         });
@@ -105,20 +119,53 @@ public class ProducersActivity extends AppCompatActivity {
                 return false;
             }
         });
+        restoreLastSearch();
         return true;
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outBundle) {
+        super.onSaveInstanceState(outBundle);
+        if (mSearchView != null && !mSearchView.isIconified()) {
+            String query = mSearchView.getQuery().toString().trim();
+            if (!TextUtils.isEmpty(query)) {
+                outBundle.putString(KEY_QUERY, query);
+            }
+        }
+    }
+
+    private boolean searchviewContainsQuery() {
+        return mSearchView != null && !TextUtils.isEmpty(mSearchView.getQuery());
+    }
+
     private void searchProducers(@NonNull String query) {
-        String cleanQuery = query.trim();
-        List<Producer> producersSearchResult = mPresenter.searchProducers(cleanQuery);
+        List<Producer> producersSearchResult = mPresenter.searchProducers(query);
         mProducersListFragment.setScrollDownToLoadNextEnable(false);
         mProducersListFragment.displayProducers(producersSearchResult);
+    }
+
+    private void restoreLastSearch() {
+        if (mSearchView != null) {
+            String query = null;
+            if (searchviewContainsQuery()) {
+                query = mSearchView.getQuery().toString();
+            } else if (mLastQuery != null) {
+                query = mLastQuery;
+                mLastQuery = null;
+            }
+            if (query != null) {
+                mSearchView.setIconified(false);
+                mSearchView.setQuery(query, true);
+            }
+        }
     }
 
     private class ProducersListenerImpl implements ProducersListener {
         @Override
         public void onNewProducersLoaded(@NonNull List<Producer> producers) {
-            mProducersListFragment.displayProducers(producers);
+            if (!searchviewContainsQuery()) {
+                mProducersListFragment.displayProducers(producers);
+            }
         }
 
         @Override
